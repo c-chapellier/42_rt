d4be194b0fbb8661159027dda459a17b7d660d8a
gautier kiss <gkgkgk395@gmail.com>
2021-10-03 19:04:19 +0200

    camera ok

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,45 +2,45 @@
 
 void test()
 {
-    printf("%f\n", RADIAN(180));
-    printf("%f\n", RADIAN(360));
+    // printf("%f\n", RADIAN(180));
+    // printf("%f\n", RADIAN(360));
 
-    printf("%f\n", sin(RADIAN(0)));
-    printf("%f\n", sin(RADIAN(30)));
-    printf("%f\n", sin(RADIAN(45)));
-    printf("%f\n", sin(RADIAN(60)));
-    printf("%f\n", sin(RADIAN(90)));
-    printf("%f\n", sin(RADIAN(180)));
-    printf("%f\n", sin(RADIAN(270)));
+    // printf("%f\n", sin(RADIAN(0)));
+    // printf("%f\n", sin(RADIAN(30)));
+    // printf("%f\n", sin(RADIAN(45)));
+    // printf("%f\n", sin(RADIAN(60)));
+    // printf("%f\n", sin(RADIAN(90)));
+    // printf("%f\n", sin(RADIAN(180)));
+    // printf("%f\n", sin(RADIAN(270)));
 
-    printf("%f\n", DEGREE(asin(sin(RADIAN(0)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(30)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(45)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(60)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(90)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(180)))));
-    printf("%f\n", DEGREE(asin(sin(RADIAN(270)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(0)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(30)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(45)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(60)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(90)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(180)))));
+    // printf("%f\n", DEGREE(asin(sin(RADIAN(270)))));
 
-    Vector *v1 = new Vector(1, 0, 0); // + 0
-    Vector *v2 = new Vector(1, 1, 0); // + +
-    Vector *v3 = new Vector(0, 1, 0); // 0 +
-    Vector *v4 = new Vector(-1, 1, 0); // - +
-    Vector *v5 = new Vector(-1, 0, 0); // - 0
-    Vector *v6 = new Vector(-1, -1, 0); // - -
-    Vector *v7 = new Vector(0, -1, 0); // 0 -
-    Vector *v8 = new Vector(1, -1, 0); // + -
+    // Vector *v1 = new Vector(1, 0, 0); // + 0
+    // Vector *v2 = new Vector(1, 1, 0); // + +
+    // Vector *v3 = new Vector(0, 1, 0); // 0 +
+    // Vector *v4 = new Vector(-1, 1, 0); // - +
+    // Vector *v5 = new Vector(-1, 0, 0); // - 0
+    // Vector *v6 = new Vector(-1, -1, 0); // - -
+    // Vector *v7 = new Vector(0, -1, 0); // 0 -
+    // Vector *v8 = new Vector(1, -1, 0); // + -
 
-    std::cout << "angle between v1 and v1 (0): " << v1->angleWith(v1) << " sin: " << sin(RADIAN(v1->angleWith(v1))) << " cos: " << cos(RADIAN(v1->angleWith(v1))) <<  " dir: " << v1->directionXY(v1) << std::endl;
-    std::cout << "angle between v1 and v2 (45): " << v1->angleWith(v2) << " sin: " << sin(RADIAN(v1->angleWith(v2))) << " cos: " << cos(RADIAN(v1->angleWith(v2))) << " dir: " << v1->directionXY(v2) << std::endl;
-    std::cout << "angle between v1 and v3 (90): " << v1->angleWith(v3) << " sin: " << sin(RADIAN(v1->angleWith(v3))) << " cos: " << cos(RADIAN(v1->angleWith(v3))) << " dir: " << v1->directionXY(v3) << std::endl;
-    std::cout << "angle between v1 and v4 (135): " << v1->angleWith(v4) << " sin: " << sin(RADIAN(v1->angleWith(v4))) << " cos: " << cos(RADIAN(v1->angleWith(v4))) << " dir: " << v1->directionXY(v4) << std::endl;
-    std::cout << "angle between v1 and v5 (180): " << v1->angleWith(v5) << " sin: " << sin(RADIAN(v1->angleWith(v5))) << " cos: " << cos(RADIAN(v1->angleWith(v5))) << " dir: " << v1->directionXY(v5) << std::endl;
-    std::cout << "angle between v1 and v6 (225): " << v1->angleWith(v6) << " sin: " << sin(RADIAN(v1->angleWith(v6))) << " cos: " << cos(RADIAN(v1->angleWith(v6))) << " dir: " << v1->directionXY(v6) << std::endl;
-    std::cout << "angle between v1 and v7 (270): " << v1->angleWith(v7) << " sin: " << sin(RADIAN(v1->angleWith(v7))) << " cos: " << cos(RADIAN(v1->angleWith(v7))) << " dir: " << v1->directionXY(v7) << std::endl;
-    std::cout << "angle between v1 and v8 (315): " << v1->angleWith(v8) << " sin: " << sin(RADIAN(v1->angleWith(v8))) << " cos: " << cos(RADIAN(v1->angleWith(v8))) << " dir: " << v1->directionXY(v8) << std::endl;
+    // std::cout << "angle between v1 and v1 (0): " << v1->angleWith(v1) << " sin: " << sin(RADIAN(v1->angleWith(v1))) << " cos: " << cos(RADIAN(v1->angleWith(v1))) <<  " dir: " << v1->directionXY(v1) << std::endl;
+    // std::cout << "angle between v1 and v2 (45): " << v1->angleWith(v2) << " sin: " << sin(RADIAN(v1->angleWith(v2))) << " cos: " << cos(RADIAN(v1->angleWith(v2))) << " dir: " << v1->directionXY(v2) << std::endl;
+    // std::cout << "angle between v1 and v3 (90): " << v1->angleWith(v3) << " sin: " << sin(RADIAN(v1->angleWith(v3))) << " cos: " << cos(RADIAN(v1->angleWith(v3))) << " dir: " << v1->directionXY(v3) << std::endl;
+    // std::cout << "angle between v1 and v4 (135): " << v1->angleWith(v4) << " sin: " << sin(RADIAN(v1->angleWith(v4))) << " cos: " << cos(RADIAN(v1->angleWith(v4))) << " dir: " << v1->directionXY(v4) << std::endl;
+    // std::cout << "angle between v1 and v5 (180): " << v1->angleWith(v5) << " sin: " << sin(RADIAN(v1->angleWith(v5))) << " cos: " << cos(RADIAN(v1->angleWith(v5))) << " dir: " << v1->directionXY(v5) << std::endl;
+    // std::cout << "angle between v1 and v6 (225): " << v1->angleWith(v6) << " sin: " << sin(RADIAN(v1->angleWith(v6))) << " cos: " << cos(RADIAN(v1->angleWith(v6))) << " dir: " << v1->directionXY(v6) << std::endl;
+    // std::cout << "angle between v1 and v7 (270): " << v1->angleWith(v7) << " sin: " << sin(RADIAN(v1->angleWith(v7))) << " cos: " << cos(RADIAN(v1->angleWith(v7))) << " dir: " << v1->directionXY(v7) << std::endl;
+    // std::cout << "angle between v1 and v8 (315): " << v1->angleWith(v8) << " sin: " << sin(RADIAN(v1->angleWith(v8))) << " cos: " << cos(RADIAN(v1->angleWith(v8))) << " dir: " << v1->directionXY(v8) << std::endl;
 
-    Camera c(0, 0, 0, 0, 0, 1, 90);
-    c.getScreen(10, 10);
+    // Camera c(0, 0, 0, 1, 1, 1, 90);
+    // c.getScreen(10, 10);
 }
 
 int main(void)
--- srcs/visuals/Camera.cpp
+++ srcs/visuals/Camera.cpp
@@ -2,6 +2,8 @@
 
 Camera::Camera(double px, double py, double pz, double vx, double vy, double vz, double a)
 {
+    if(vx == 0 && vy == 0 && vz == 0)
+        throw "The camera vector can not be the null vector";
     this->p = new Point(px, py, pz);
     this->v = new Vector(vx, vy, vz);
     if (a < 60 || a > 120)
@@ -10,6 +12,8 @@ Camera::Camera(double px, double py, double pz, double vx, double vy, double vz,
 }
 Camera::Camera(Point p, Vector v, double a)
 {
+    if(v.getX() == 0 && v.getY() == 0 && v.getZ() == 0)
+        throw "The camera vector can not be the null vector";
     this->p = new Point(p);
     this->v = new Vector(v);
     this->angle = a;
@@ -41,91 +45,96 @@ void Camera::getScreen(int width, int height)
     }
     std::cout << std::endl;std::cout << std::endl;
 
-    // calc
+    // vectors
     Vector *x_axis = new Vector(1, 0, 0); // X axis
-    Vector *z_axis = new Vector(0, 0, 1); // Z axis
+    Vector *projection_on_plane_xy = new Vector(this->v->getX(), this->v->getY(), 0);
+    Vector *projection_on_plane_xz = new Vector(this->v->getX(), 0, this->v->getZ());
 
     // angle between the camera vector and X axis
-    double alpha = this->v->angleWith(x_axis);
+    double alpha = projection_on_plane_xy->angleWith(x_axis);
     // if right turn angle = one complete turn minus himself (trigonometric circle)
-    alpha = this->v->directionXY(x_axis) == CLOCK_WISE ? (360 - alpha) : (alpha);
+    alpha = projection_on_plane_xy->directionXY(x_axis) == CLOCK_WISE ? (360 - alpha) : (alpha);
     // angle between the camera vector and Z axis
-    double gama = 90 - this->v->angleWith(z_axis);
-    // ???
-    gama = this->v->directionXZ(z_axis) == CLOCK_WISE ? (360 - gama) : (gama);
+    double gama = projection_on_plane_xz->angleWith(x_axis);
+    // if right turn angle = one complete turn minus himself (trigonometric circle)
+    gama = projection_on_plane_xz->directionXZ(x_axis) == CLOCK_WISE ? (360 - gama) : (gama);
 
 
     for (int z = 0; z < height; ++z) {
         for (int y = 0; y < width; ++y) {
             std::cout << std::endl << "Point: " << screen[z][y] << std::endl;
-            Point *new_point; // res
+            Point *new_point = new Point(screen[z][y]); // res
+            double new_x, new_y, new_z, new_angle, beta, dist;
 
-            // create a new point on Z axis at heigh of the current point
-            Point *ooz = new Point(0, 0, - z + double(height / 2));
-            // create a vector from the previous point to current point
-            Vector *p_ooz = new Vector(*ooz, screen[z][y]);
-            // get the distance between the current point and point on Z axis (R of polar coordinates)
-            double dist = screen[z][y].distWith(*ooz);
-            // get the angle between the previous vector and X axis (α of polar coordinates)
-            double beta = p_ooz->angleWith(x_axis);
-            // if right turn angle = one complete turn minus himself (trigonometric circle)
-            beta = p_ooz->directionXY(x_axis) == CLOCK_WISE ? (360 - beta) : (beta);
-            // the final angle is the current angle + X axis rotation
-            // ex: (50 120 = > 170) : (50 320 => 370 => 10) : (320 350 => 670 => 310)
-            double new_angle = beta + alpha;
-            
-            
-            // new coordinates
-            double new_x = cos(RADIAN(new_angle)) * dist;
-            double new_y = sin(RADIAN(new_angle)) * dist; // reverse the sin because the repere X Y is invert of trigonometric circle
-            double new_z = screen[z][y].getZ();
+            if(projection_on_plane_xy->getMagnitude() != 0) {
+                // create a new point on Z axis at heigh of the current point
+                Point *ooz = new Point(0, 0, - z + double(height / 2));
+                // create a vector from the previous point to current point
+                Vector *p_ooz = new Vector(*ooz, screen[z][y]);
+                // get the distance between the current point and point on Z axis (R of polar coordinates)
+                dist = screen[z][y].distWith(*ooz);
+                // get the angle between the previous vector and X axis (α of polar coordinates)
+                beta = p_ooz->angleWith(x_axis);
+                // if right turn angle = one complete turn minus himself (trigonometric circle)
+                beta = p_ooz->directionXY(x_axis) == CLOCK_WISE ? (360 - beta) : (beta);
+                // the final angle is the current angle + X axis rotation
+                // ex: (50 120 = > 170) : (50 320 => 370 => 10) : (320 350 => 670 => 310)
+                new_angle = beta + alpha;
+                
+                // new coordinates
+                new_x = cos(RADIAN(new_angle)) * dist;
+                new_y = sin(RADIAN(new_angle)) * dist; // reverse the sin because the repere X Y is invert of trigonometric circle
+                new_z = screen[z][y].getZ();
 
-            std::cout << "dist: " << dist << std::endl;
-            std::cout << "alpha: " << alpha << std::endl;
-            std::cout << "beta: " << beta << std::endl;
-            std::cout << "clock_wise: " << p_ooz->directionXY(x_axis) << std::endl;
-            std::cout << "new_angle: " << new_angle << std::endl;
-            std::cout << "sin: " << sin(RADIAN(new_angle)) << std::endl;
-            std::cout << "cos: " << cos(RADIAN(new_angle)) << std::endl;
-            std::cout << "x: " << new_x << " y: " << new_y << " z: " << new_z << std::endl;
+                // std::cout << "dist: " << dist << std::endl;
+                // std::cout << "alpha: " << alpha << std::endl;
+                // std::cout << "beta: " << beta << std::endl;
+                // std::cout << "clock_wise: " << p_ooz->directionXY(x_axis) << std::endl;
+                // std::cout << "new_angle: " << new_angle << std::endl;
+                // std::cout << "sin: " << sin(RADIAN(new_angle)) << std::endl;
+                // std::cout << "cos: " << cos(RADIAN(new_angle)) << std::endl;
+                // std::cout << "x: " << new_x << " y: " << new_y << " z: " << new_z << std::endl;
 
-            // create a vector from the current point to the new point
-            Vector v1(screen[z][y], new_x, new_y, new_z);
-            // apply this vector to the current point
-            new_point = screen[z][y].applyVector(&v1);
+                // create a vector from the current point to the new point
+                Vector v1(screen[z][y], new_x, new_y, new_z);
+                // apply this vector to the current point
+                new_point = screen[z][y].applyVector(&v1);
 
-            std::cout << "actual point: " << screen[z][y] << std::endl;
-            std::cout << "v1: " << v1 << std::endl;
-            std::cout << "new point: " << *new_point << std::endl;
+                // std::cout << "actual point: " << screen[z][y] << std::endl;
+                // std::cout << "v1: " << v1 << std::endl;
+                // std::cout << "new point: " << *new_point << std::endl;
+            }
 
-            Point *oyo = new Point(0, y - double(width / 2), 0);
-            Vector *p_oyo = new Vector(*oyo, screen[z][y]);
-            dist = screen[z][y].distWith(*oyo); // dist
-            beta = p_oyo->angleWith(x_axis);
-            beta = p_oyo->directionXZ(x_axis) == CLOCK_WISE ? (beta) : (360 - beta);
-            new_angle = beta + gama;
+            if(projection_on_plane_xz->getMagnitude() != 0) {
+                Point *oyo = new Point(0, y - double(width / 2), 0);
+                Vector *p_oyo = new Vector(*oyo, screen[z][y]);
+                dist = screen[z][y].distWith(*oyo); // dist
+                beta = p_oyo->angleWith(x_axis);
+                beta = p_oyo->directionXZ(x_axis) == CLOCK_WISE ? (beta) : (360 - beta);
+                new_angle = beta + gama;
 
-            // new coordinates
-            new_x = cos(RADIAN(new_angle)) * dist;
-            new_y = screen[z][y].getY();
-            new_z = sin(RADIAN(new_angle)) * dist;
+                // new coordinates
+                new_x = cos(RADIAN(new_angle)) * dist;
+                new_y = screen[z][y].getY();
+                new_z = sin(RADIAN(new_angle)) * dist;
 
-            std::cout << "dist: " << dist << std::endl;
-            std::cout << "gama: " << gama << std::endl;
-            std::cout << "beta: " << beta << std::endl;
-            std::cout << "clock_wise: " << p_oyo->directionXZ(x_axis) << std::endl;
-            std::cout << "new_angle: " << new_angle << std::endl;
-            std::cout << "sin: " << sin(RADIAN(new_angle)) << std::endl;
-            std::cout << "cos: " << cos(RADIAN(new_angle)) << std::endl;
-            std::cout << "x: " << new_x << " y: " << new_y << " z: " << new_z << std::endl;
+                // std::cout << "dist: " << dist << std::endl;
+                // std::cout << "gama: " << gama << std::endl;
+                // std::cout << "beta: " << beta << std::endl;
+                // std::cout << "clock_wise: " << p_oyo->directionXZ(x_axis) << std::endl;
+                // std::cout << "new_angle: " << new_angle << std::endl;
+                // std::cout << "sin: " << sin(RADIAN(new_angle)) << std::endl;
+                // std::cout << "cos: " << cos(RADIAN(new_angle)) << std::endl;
+                // std::cout << "x: " << new_x << " y: " << new_y << " z: " << new_z << std::endl;
 
-            Vector v2(screen[z][y], new_x, new_y, new_z);
-            new_point = new_point->applyVector(&v2);
+                Vector v2(screen[z][y], new_x, new_y, new_z);
+                new_point = new_point->applyVector(&v2);
 
-            std::cout << "actual point: " << screen[z][y] << std::endl;
-            std::cout << "v2: " << v2 << std::endl;
-            std::cout << "new point: " << *new_point << std::endl;
-            
+                // std::cout << "actual point: " << screen[z][y] << std::endl;
+                // std::cout << "v2: " << v2 << std::endl;
+                // std::cout << "new point: " << *new_point << std::endl;
+            }
+
             screen[z][y].setX(new_point->getX() + this->p->getX());
             screen[z][y].setY(new_point->getY() + this->p->getY());
             screen[z][y].setZ(new_point->getZ() + this->p->getZ());

093d4f939834407ceec59d516db2b7761a17bba5
gautier kiss <gkgkgk395@gmail.com>
2021-11-06 12:48:10 +0100

    Gradient on circle

--- src/objects/Circle.cpp
+++ src/objects/Circle.cpp
@@ -5,6 +5,8 @@ Circle::Circle(double px, double py, double pz, double vx, double vy, double vz,
     this->plane = new Plane(px, py, pz, vx, vy, vz);
     this->R = R;
     this->r = r;
+    if(r >= R)
+        throw "Impossible circle";
 }
 Circle::~Circle() {
     delete(this->plane);
@@ -27,17 +29,17 @@ double Circle::angleWith(const Line &line) const
 
 Color Circle::getColorAt(int height, int width, int screen_height, int screenWidth, const Point &intersection) const
 {
-    screenWidth = intersection.getX();
+    screenWidth = screen_height;
 
     if (this->texture.getType() == "Uniform") {
         return this->getColor();
     } else if (this->texture.getType() == "Gradient") {
-        double p = (double)((double)height / (double)screen_height);
+        double ratio = (intersection.distWith(this->plane->getP()) - this->r) / (this->R - this->r);
         return Color(
-            this->getColor(0).getR() + (int)((double)((double)this->getColor(1).getR() - (double)this->getColor(0).getR()) * p),
-            this->getColor(0).getG() + (int)((double)((double)this->getColor(1).getG() - (double)this->getColor(0).getG()) * p),
-            this->getColor(0).getB() + (int)((double)((double)this->getColor(1).getB() - (double)this->getColor(0).getB()) * p),
-            this->getColor(0).getO() + (int)((double)((double)this->getColor(1).getO() - (double)this->getColor(0).getO()) * p)
+            this->getColor(0).getR() + (int)((double)((double)this->getColor(1).getR() - (double)this->getColor(0).getR()) * ratio),
+            this->getColor(0).getG() + (int)((double)((double)this->getColor(1).getG() - (double)this->getColor(0).getG()) * ratio),
+            this->getColor(0).getB() + (int)((double)((double)this->getColor(1).getB() - (double)this->getColor(0).getB()) * ratio),
+            this->getColor(0).getO() + (int)((double)((double)this->getColor(1).getO() - (double)this->getColor(0).getO()) * ratio)
         );
     } else if (this->texture.getType() == "Grid") {
         return this->getColor(((height / this->texture.getValue1()) + (width / this->texture.getValue2())) % 2);
